5ee115fc71cea9e4556a70ed03ecc78070899b0a
Manuel Sanchez Suyon <manueduardo000@hotmail.com>
2022-12-06 22:25:37 -0500

    Modifique index.php

--- src/php/index.php
+++ src/php/index.php
@@ -9,20 +9,6 @@
 </head>
 
 <body>
-docker run -p 4000:80 -d -v "$(pwd)/src:/var/www/html/" hello-php:1.0
-    <h1><?php echo "PHP PARA PROYECTO" ?></h1>
-
-    <form action="calculadora.php" method="post">
-        <p>Numero 1: <input type="text" name="numero1" /></p>
-        <p>Numero 2: <input type="text" name="numero2" /></p>
-        <ul>
-            <li>Ingrese 1 para sumar</li>
-            <li>Ingrese 2 para restar</li>
-            <li>Ingrese 3 para multiplicar</li>
-            <li>Ingrese 4 para dividir</li>
-        </ul>
-        <p>Operación : <input type="text" name="operacion" /></p>
-        <p><input type="submit" /></p>
-    </form>
+    
 </body>
 </html>
